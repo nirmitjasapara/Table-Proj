dba00e4ab4421c7460e51b95df6ae329261effed
nirmit jasapara <nirmit.jasapara@gmail.com>
2021-10-29 16:56:11 -0700

    changed the banckend back

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api"
-  //API_ENDPOINT: "http://table-proj-server.herokuapp.com/api"
+  //API_ENDPOINT: "http://localhost:8000/api"
+  API_ENDPOINT: "http://table-proj-server.herokuapp.com/api"
 };
